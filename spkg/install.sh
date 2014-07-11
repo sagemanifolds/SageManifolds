115111ee40d36b0c6cfdad761e3048a3ef21f858
Michal Bejger <bejger@camk.edu.pl>
2014-07-11 18:39:53 +0200

    Altering src/doc/en/reference/index.rst

--- spkg/install.sh
+++ spkg/install.sh
@@ -12,7 +12,6 @@ echo "$(tput setaf 4)Unpacking...$(tput sgr 0)"
 tar -zxvf manifolds-0.5.tar.gz
 
 # altering the src/sage/all.py for import 
-
 if [[ -z $(grep "tensor.modules.all" src/sage/all.py) ]]; then
    
    echo "$(tput setaf 4)Adding tensor.modules import call to all.py...$(tput sgr 0)"
@@ -25,8 +24,23 @@ if [[ -z $(grep "geometry.manifolds.all" src/sage/all.py) ]]; then
    echo "from sage.geometry.manifolds.all import *" >> src/sage/all.py
 fi 
 
+# altering src/doc/en/reference/index.rst for documentation
+if [[ -z $(grep "tensor_free_modules/index" src/doc/en/reference/index.rst) ]]; then
+    
+    echo "$(tput setaf 4)Adding tensor free modules documentation to index.rst...$(tput sgr 0)"
+    sed -i '/<modules\/index>/a* :doc:`Tensors on free modules of finite rank <tensor_free_modules\/index>`' src/doc/en/reference/index.rst
 
-echo -e "\n$(tput bold)$(tput setaf 4)Done! Run ./sage -b to re-build sage.$(tput sgr 0)"
+fi
+
+if [[ -z $(grep "manifolds/index" src/doc/en/reference/index.rst) ]]; then
+
+    echo "$(tput setaf 4)Adding manifolds documentation to index.rst...$(tput sgr 0)"
+    sed -i '/<tensor\/index>/a* :doc:`Differential Geometry <manifolds\/index>`' src/doc/en/reference/index.rst
+
+fi
+
+echo -e "\n$(tput bold)$(tput setaf 4)Done! Running ./sage -b to re-build sage.$(tput sgr 0)"
+./sage -b 
 
 exit 0
 
