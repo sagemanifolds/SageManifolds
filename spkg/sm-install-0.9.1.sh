4bb80bcf9e7388d5b91e538afcff2b85c23fb300
Eric Gourgoulhon <eric.gourgoulhon@obspm.fr>
2016-09-15 13:50:53 +0200

    Update scripts for 0.9.1

--- spkg/sm-install-0.9.1.sh
+++ spkg/sm-install-0.9.1.sh
@@ -2,7 +2,6 @@
 
 if [ "$1" = "doc" ]; then
     echo -e "\n$(tput bold)$(tput setaf 4)Building the html documentation (may take some time).$(tput sgr 0)"
-    ./sage -docbuild reference/parallel html
     ./sage -docbuild reference/tensor_free_modules html
     ./sage -docbuild reference/manifolds html
     echo -e "\n$(tput setaf 4)NB: if some error in building the documention occured,$(tput sgr 0)"
@@ -52,7 +51,6 @@ tar -zxvf manifolds-0.9.1.tar.gz
 touch src/sage/manifolds/*.py
 touch src/sage/manifolds/differentiable/*.py
 touch src/sage/tensor/modules/*.py
-touch src/sage/parallel/*.py
 echo -e "\n$(tput bold)$(tput setaf 4)Running ./sage -b to re-build Sage.$(tput sgr 0)"
 ./sage -b
 echo -e "\n$(tput bold)$(tput setaf 4)Installation of SageManifolds 0.9.1 completed!\n$(tput sgr 0)"
